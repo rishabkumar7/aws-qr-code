04307671fa900b3443de5e3a52a9195ae90eb366
rishabkumar7 <rishabkumar7@gmail.com>
2023-09-10 22:25:33 -0400

    Added Lambda Function with packages

---
+++ lambda_function/lambda_function.py
@@ -0,0 +1,35 @@
+import json
+import boto3
+import qrcode
+import io
+import base64
+
+# Initialize a session using Amazon S3
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    # Parse the URL from the event
+    url = event['url']
+    
+    # Generate QR code
+    img = qrcode.make(url)
+    img_bytes = io.BytesIO()
+    img.save(img_bytes)
+    img_bytes = img_bytes.getvalue()
+    
+    # Generate a unique filename
+    filename = url.split("://")[1].replace("/", "_") + '.png'
+    
+    # Upload the QR code to the S3 bucket
+    s3.put_object(Bucket='qr-code-generator7', Key=filename, Body=img_bytes, ContentType='image/png', ACL='public-read')
+    
+    # Generate the URL of the uploaded QR code
+    location = s3.get_bucket_location(Bucket='qr-code-generator7')['LocationConstraint']
+    region = '' if location is None else f'{location}'
+    qr_code_url = f"https://s3-{region}.amazonaws.com/{'qr-code-generator7'}/{filename}"
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'QR code generated and uploaded to S3 bucket successfully!', 'qr_code_url': qr_code_url})
+    }
+
